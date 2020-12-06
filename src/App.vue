c5ea0dccf1109715ca50dd1d076bbe5820707e5a
Juan Sebastián Villegas J <juanvjaramillo@gmail.com>
2020-12-05 22:41:33 -0500

    Campos dinamicos en tarjetas. Cards V1.0

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,12 @@
 <template>
   <div id="app">
-
-
-        <div class="container-fluid">
-        <div class="row justify-content-center mb-5">
-          <div class="col mt-5" v-for="(item, index) of members" :key="index">
-            <team-card></team-card>
-          </div>
-        </div>
-        </div>
+    <div class="container-fluid">
+    <div class="row justify-content-center mb-5">
+      <div class="col mt-5" v-for="(item, index) of members" :key="index">
+        <team-card v-bind:member="item"></team-card>
+      </div>
+    </div>
+    </div>
   </div>
 </template>
 
@@ -26,42 +24,43 @@ export default {
 
   data(){
     return{
-      members : [
-        {
-​           codigo​: 1​,
-​           nombre​: ​'Lucas Mera',
-​           descripcion​: ​'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris condimentum ac elit et accumsan.'​,
-​           rol​: ​'Desarrollador backend'​,
-​           image​: ​'https://placeimg.com/192/192/people' 
+      title: 'Prueba que Vue funciona',
+      members: [
+         {
+​          codigo​: ​1​,
+​          nombre​: ​'Vanessa Aguado'​,
+​          descripcion​: ​'Tengo 26 años, soy profesional en negocios internacionales.'​,
+​          rol​: ​'Analista de software, arquitecto de software y tester.'​,
+​          image​: ​'./img/1.jpg' 
         },
         {​ 
-          codigo​: 2,
-​          nombre​: ​'Lucas Mera'​,
-​          descripcion​: ​'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris condimentum ac elit et accumsan.'​,
-​           rol​: ​'Desarrollador backend'​,
-​           image​: ​'https://placeimg.com/192/192/people' 
+          codigo​: ​2​,
+​          nombre​: ​'Álvaro Peralta'​,
+​          descripcion​: ​'Tengo 28 años, soy programador.'​,
+​           rol​: ​'Desarrollador frontend'​,
+​           image​: ​'./img/2.jpg'
         },
         {​ 
           codigo​: ​3​,
-​          nombre​: ​'Lucas Mera'​,
-​          descripcion​: ​'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris condimentum ac elit et accumsan.'​,
-​           rol​: ​'Desarrollador backend'​,
-​           image​: ​'https://placeimg.com/192/192/people' 
+​          nombre​: ​'Sergio Pineda'​,
+​          descripcion​: ​'Tengo 21 años, soy estudiante de Odontología.'​,
+​           rol​: ​'Desarrollador frontend'​,
+​           image​: ​'./img/3.jpg'
         },
         {​ 
           codigo​: ​4,
-​          nombre​: ​'Lucas Mera'​,
-​          descripcion​: ​'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris condimentum ac elit et accumsan.'​,
+​          nombre​: ​'Juan Villegas'​,
+​          descripcion​: ​'Aficionado por la teconología y nuevo amante del desarrollo.'​,
 ​           rol​: ​'Desarrollador backend'​,
-​           image​: ​'https://placeimg.com/192/192/people' 
+​           image​: './img/4.jpg'
         },
         {​ 
           codigo​: ​5,
-​          nombre​: ​'Lucas Mera'​,
-​          descripcion​: ​'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris condimentum ac elit et accumsan.'​,
-​           rol​: ​'Desarrollador backend'​,
-​           image​: ​'https://placeimg.com/192/192/people' 
-        },
+​          nombre​: ​'Diego Garzón'​,
+​          descripcion​: ​' Soy estudiante de Ingeniería Catastral y Geodesia.'​,
+​           rol​: ​'Desarrollador frontend'​,
+​           image​: ​'./img/5.jpg' 
+        }
       ],
     }
   }
