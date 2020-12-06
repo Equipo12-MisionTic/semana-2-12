9781bad29b04422886e591bf2bae7e10a35199c3
Kelpie Maiden <60309250+kelpie-maiden@users.noreply.github.com>
2020-12-05 23:50:57 -0500

    seccion contactos creada

--- src/components/TeamCard.vue
+++ src/components/TeamCard.vue
@@ -1,18 +1,34 @@
 
 
 <template>
-    <div>
-
-    </div>
+  
+    
+          <div class=" card text-white bg-dark">
+            <div class="d-flex justify-content-center p-2">
+              <img v-bind:src="member.imagen" alt="imagen del miembro">
+            </div>
+            <div class="card-body">
+              <h3 class="card-title">{{member.nombre}}</h3>
+              <p class="card-text"> Código:{{ member.codigo}}</p>
+              <p class="card-text">{{ member.descripcion}}</p>
+              <p class="card-text"> Rol:{{ member.rol}}</p>
+              
+            </div>
+            <div class="card-footer">
+              <small class="text-muted">Last updated 3 mins ago</small>
+            </div>
+          </div>
+    
+    
 </template>
 
 <script>
-    export default {
-        name: "TeamCard",
-        props: ['member']
-    }
+export default {
+  name: "TeamCard",
+  props: ["member"],
+};
 </script>
 
 <style scoped>
-
+ 
 </style>
