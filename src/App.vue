2b6ccbc265c1f70d52adeb61617622e253888d78
Juan Sebastián Villegas J <juanvjaramillo@gmail.com>
2020-12-05 20:14:00 -0500

    Estructura del componenete en app.vue y TeamCard. Cards V0.4

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,12 @@
 <template>
   <div id="app">
+
+
+        <div class="container-fluid">
+        <div class="row justify-content-center mb-5">
+          
+        </div>
+        </div>
   </div>
 </template>
 
