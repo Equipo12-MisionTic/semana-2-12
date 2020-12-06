e28a1ac3a3d68f66d5e41c34dd9ce77796c050ed
Juan Sebastián Villegas J <juanvjaramillo@gmail.com>
2020-12-05 20:42:26 -0500

    Estructura de objetos 'team' creados. Cards V0.5

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@
 
         <div class="container-fluid">
         <div class="row justify-content-center mb-5">
-          
+
         </div>
         </div>
   </div>
@@ -14,6 +14,49 @@
 
 export default {
   name: 'App',
-}
+
+
+  data(){
+    return{
+      team : [
+        {
+​          codigo​: ​1​,
+​          nombre​: ​'Lucas Mera'​,
+​          descripcion​: ​'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris condimentum ac elit et accumsan.'​,
+​          rol​: ​'Desarrollador backend'​,
+​          image​: ​'https://placeimg.com/192/192/people' 
+        },
+        {​ 
+          codigo​: ​2​,
+​          nombre​: ​'Lucas Mera'​,
+​          descripcion​: ​'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris condimentum ac elit et accumsan.'​,
+​           rol​: ​'Desarrollador backend'​,
+​           image​: ​'https://placeimg.com/192/192/people' 
+        },
+        {​ 
+          codigo​: ​3​,
+​          nombre​: ​'Lucas Mera'​,
+​          descripcion​: ​'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris condimentum ac elit et accumsan.'​,
+​           rol​: ​'Desarrollador backend'​,
+​           image​: ​'https://placeimg.com/192/192/people' 
+        },
+        {​ 
+          codigo​: ​4,
+​          nombre​: ​'Lucas Mera'​,
+​          descripcion​: ​'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris condimentum ac elit et accumsan.'​,
+​           rol​: ​'Desarrollador backend'​,
+​           image​: ​'https://placeimg.com/192/192/people' 
+        },
+        {​ 
+          codigo​: ​15,
+​          nombre​: ​'Lucas Mera'​,
+​          descripcion​: ​'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris condimentum ac elit et accumsan.'​,
+​           rol​: ​'Desarrollador backend'​,
+​           image​: ​'https://placeimg.com/192/192/people' 
+        },
+      ],
+    }
+  }
+  
 </script>
 
