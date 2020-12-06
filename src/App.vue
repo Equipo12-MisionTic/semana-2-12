f53df404f9633b1b2bc58736f9439f5cbdb79a8d
SergioPinedaM7 <sergiopinedamejia@gmail.com>
2020-12-05 21:13:25 -0500

    seccion banner

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,19 @@
 <template>
   <div id="app">
+    <Banner></Banner>
   </div>
 </template>
 
 <script>
+import Banner from './components/Banner.vue'
+
+
 
 export default {
   name: 'App',
+  components:{
+    Banner
+  },
 }
 </script>
 
