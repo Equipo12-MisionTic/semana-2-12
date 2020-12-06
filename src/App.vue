2a01875e75e29aa6fa3b11f790f5b7cc38d4d1f3
alanpepa <alanpepa92@gmail.com>
2020-12-05 19:53:12 -0500

    creación del folder

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,18 @@
 <template>
   <div id="app">
+    <page-footer></page-footer>
   </div>
 </template>
 
 <script>
 
+import PageFooter from './components/PageFooter.vue'
+
 export default {
   name: 'App',
+  components:{
+    PageFooter
+  }
 }
 </script>
 
